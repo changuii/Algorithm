5520713100731eaa4be2c497267bf6704b4b33ac
changuii <rhljh7410@gmail.com>
2024-01-22 17:46:29 +0900

    14425

---
+++ study/backjun_14425/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<N; i++){
+            map.put(br.readLine(), 1);
+        }
+
+        int answer = 0;
+        for(int i=0; i<M; i++){
+            if(map.containsKey(br.readLine())) answer++;
+        }
+
+        System.out.println(answer);
+
+    }
+}
