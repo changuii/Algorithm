19737fb9411e9733686da3f1e227549bc4d93a55
changui <122252160+changuii@users.noreply.github.com>
2024-01-25 22:29:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 700 ms, Memory: 141780 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,59 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean[] V;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken())+1;
         int M = Integer.parseInt(st.nextToken());
-        Stack<Integer> stack = new Stack<>();
 
-        boolean[] V = new boolean[N];
-        LinkedList<Integer>[] list = new LinkedList[N];
-
-        for(int i=0; i<N; i++){
-            list[i] = new LinkedList<>();
-        }
-
-        for(int i=1; i<M+1; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list[x].add(y);
-            list[y].add(x);
-        }
+        A = new ArrayList[N];
+        V = new boolean[N];
         for(int i=1; i<N; i++){
-            V[i] = false;
+            A[i] = new ArrayList<>();
         }
-        int answer = 0;
 
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        int answer = 0;
         for(int i=1; i<N; i++){
             if(!V[i]){
                 answer++;
-                V[i] = true;
-                while(!list[i].isEmpty()){
-                    if(!V[list[i].getFirst()])
-                        stack.push(list[i].removeFirst());
-                }
-                while (!stack.isEmpty()){
-                    int next = stack.pop();
-                    V[next] = true;
-                    while (!list[next].isEmpty()){
-                        stack.push(list[next].removeFirst());
-                    }
-
-                }
+                DFS(i);
             }
         }
         System.out.println(answer);
-
-
     }
-
+    public static void DFS(int v){
+        if(V[v]){
+            return;
+        }
+        V[v] = true;
+        for(int x : A[v]){
+            if(!V[x])
+                DFS(x);
+        }
+    }
 }
