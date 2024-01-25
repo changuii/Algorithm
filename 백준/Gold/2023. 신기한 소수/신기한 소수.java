25a073325016e0b1c5f57dfb26fcb04913c333d7
changui <122252160+changuii@users.noreply.github.com>
2024-01-25 22:54:23 +0900

    [Gold V] Title: 신기한 소수, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,46 +1,59 @@
-
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
+	static int N;
+	static int[] starts = { 2, 3, 5, 7 };
+	static int[] additionals = { 1, 3,5, 7, 9 };
+	static StringBuilder sb;
 
-        DFS(2, 1);
-        DFS(3, 1);
-        DFS(5, 1);
-        DFS(7, 1);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(sb);
+		N = Integer.parseInt(br.readLine());
 
-    }
-    public static void DFS(int number, int j){
-        if(j == N){
-            if(isPrime(number)){
-                sb.append(number).append("\n");
-            }
-            return;
-        }
-        for(int i=1; i<10; i++){
-            if(i%2 == 0){
-                continue;
-            }
-            if(isPrime(number*10 + i)){
-                DFS(number * 10 + i, j+1);
-            }
-        }
+		sb = new StringBuilder();
+		dfs(0, 0);
 
+		System.out.println(sb);
 
-    }
-    public static boolean isPrime(int num){
-        for(int i=2; i<= num/2; i++){
-            if(num % i == 0)
-                return false;
-        }
-        return true;
-    }
+		br.close();
+	}
+
+	public static void dfs(int c, int x) {
+		if (c == N) {
+			sb.append(x).append("\n");
+			return;
+		}
+		// 자리수 +1 
+		x *= 10;
+		
+		// 가장 앞의 자리수라면 2, 3, 5, 7
+		if (c == 0) {
+			for (int n : starts) {
+				int nx = x + n;
+				if (isPrime(nx)) {
+					dfs(c + 1, nx);
+				}
+			}
+		// 나머지는 1, 3, 5, 7, 9
+		} else {
+			for (int n : additionals) {
+				int nx = x + n;
+				if (isPrime(nx)) {
+					dfs(c + 1, nx);
+				}
+			}
+		}
+	}
+
+	public static boolean isPrime(int x) {
+		for (int i = 2; i <= Math.sqrt(x); i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
