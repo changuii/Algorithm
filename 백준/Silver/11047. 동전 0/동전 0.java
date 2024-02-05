3107ccc9dc35c1c32194b1c6a2595b76dec7fd5d
changui <122252160+changuii@users.noreply.github.com>
2024-02-06 01:19:55 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -5,31 +5,29 @@ import java.util.*;
 
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
-        int answer = 0;
 
         int[] A = new int[N];
-
         for(int i=0; i<N; i++){
             A[i] = Integer.parseInt(br.readLine());
         }
 
-        while (T != 0){
-            for(int i=N-1; i >=0 ; i--){
-                if(A[i] <= T){
-                    answer += T/A[i];
-                    T = T%A[i];
-                    break;
-                }
+        int answer = 0;
+        for(int i=N-1; i>=0; i--){
+            if(A[i] <= T ){
+                answer += T/A[i];
+                T = T%A[i];
             }
+
         }
+
         System.out.println(answer);
+
 
     }
 }
