0d896f469e006b5d5971da175b94bf569f7d2658
changui <122252160+changuii@users.noreply.github.com>
2024-02-07 13:01:28 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,39 +4,33 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    // 가장 큰 값을 빼는 것이 가장 이득
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine(), "-");
 
-        int N = st.countTokens();
-        String[] A = new String[N];
-        for(int i=0; i<N; i++){
+
+        String[] A = new String[st.countTokens()];
+
+        for(int i=0; i<A.length; i++){
             A[i] = st.nextToken();
         }
 
-        for(int i=0; i<N; i++){
+
+        for(int i=0; i<A.length; i++){
             st = new StringTokenizer(A[i], "+");
-            int data = 0;
+            int sum = 0;
             while (st.hasMoreTokens()){
-                data += Integer.parseInt(st.nextToken());
+                sum += Integer.parseInt(st.nextToken());
             }
-            A[i] = data+"";
+            A[i] = sum+"";
         }
 
         int answer = Integer.parseInt(A[0]);
-        for(int i=1; i<N; i++){
+        for(int i=1; i<A.length; i++){
             answer -= Integer.parseInt(A[i]);
         }
 
         System.out.println(answer);
 
-
     }
-
-
-
-
-
 }
