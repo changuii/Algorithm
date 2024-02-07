cd318ec4bf8e3ec83b1661df4021d71af9accb10
changui <122252160+changuii@users.noreply.github.com>
2024-02-07 13:17:05 +0900

    [Silver I] Title: 회의실 배정, Time: 500 ms, Memory: 42820 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -3,51 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 class Main {
-    // 종료 시간이 우선인 것을 최선으로
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-      int[][] A = new int[N][2];
-
-
-      StringTokenizer st;
-      for(int i=0; i<N; i++){
-          st = new StringTokenizer(br.readLine(), " ");
-          int start = Integer.parseInt(st.nextToken());
-          int end = Integer.parseInt(st.nextToken());
-          A[i][0] = start;
-          A[i][1] = end;
-      }
-
-      Arrays.sort(A, new Comparator<int[]>() {
-          @Override
-          public int compare(int[] o1, int[] o2) {
-              if(o1[1] == o2[1]) return o1[0] - o2[0];
-              else return o1[1] - o2[1];
-           }
-      });
-
-      int answer = 0;
-      int end = -1;
-      for(int i=0; i<N; i++){
-          if(A[i][0] >= end){
-              end = A[i][1];
-              answer++;
-          }
-      }
-
-      System.out.println(answer);
+        int[][] A = new int[N][2];
 
 
+        StringTokenizer st;
+        for(int i=0; i<N; i++){
+            st= new StringTokenizer(br.readLine(), " ");
+            A[i][0] = Integer.parseInt(st.nextToken());
+            A[i][1] = Integer.parseInt(st.nextToken());
+        }
 
+        // 종료시간이 짧은 것 우선
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) return o1[0] - o2[0];
+                else return o1[1] - o2[1];
+            }
+        });
+
+
+        int answer = 0;
+        int now = 0;
+        for(int i=0; i<N;i++){
+            if(A[i][0] >= now) {
+                answer++;
+                now = A[i][1];
+            }
+        }
+
+        System.out.println(answer);
     }
-
-
-
-
-
 }
