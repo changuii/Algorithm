eb8577487ff5cde28a44ee5070b8461978fc0d2d
changui <122252160+changuii@users.noreply.github.com>
2024-02-11 16:00:00 +0900

    [Gold V] Title: 거의 소수, Time: 232 ms, Memory: 26488 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,42 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        long N = Long.parseLong(st.nextToken());
-        long M = Long.parseLong(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
 
-        long[] A = new long[10000001];
-        for(int i= 2; i< A.length; i++){
-            A[i] = i;
-        }
-
-        for(int i=2; i<Math.sqrt(A.length); i++){
-            if(A[i] == 0) continue;
-            for(int j = i + i; j < A.length ; j += i){
-                A[j] = 0;
+        // 100,000,000,000,000 -> (10,000,000)^2 즉 천만의 제곱까지만 구하면 거의 소수를 구할 수 있다.
+        boolean[] X = new boolean[10000001];
+        for(int i=2; i<Math.sqrt(X.length); i++){
+            if(!X[i]) {
+                for (int j = i + i; j < X.length; j += i) {
+                    X[j] = true;
+                }
             }
         }
 
-
+        // 거의 소수 -> A와 B사이에 있고 소수의 제곱인 수
+        // 수가 크기때문에 수를 단순화 시킨다.
+        // N^k <= B -> N <= B / N^k-1
+        // N^k >= A -> N >= A / N^k-1
         int answer = 0;
-
-        for(int i= 2; i< A.length; i++){
-            if(A[i] != 0){
-                long temp = A[i];
-                // N^k <= B --> N <= B / N^k-1
-                while ((double)A[i] <= (double) M / (double) temp){
-                    // N^k >= A --> N >= A / N^k-1
-                    if((double)A[i] >= (double) N/ (double) temp){
+        for (int i = 2; i < X.length; i++) {
+            if (!X[i]) {
+                long now = i;
+                while ((double) i <= (double) B / (double) now) {
+                    if ((double) i >= (double) A / (double) now) {
                         answer++;
                     }
-                    // 제곱
-                    temp *= A[i];
+                    now *= i;
                 }
             }
         }
@@ -44,8 +42,6 @@ class Main {
 
 
     }
-
-
 
 
 
