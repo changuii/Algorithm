1a161b3b119be032b701d39717df6b62bdb4e5a5
changui <122252160+changuii@users.noreply.github.com>
2024-02-11 16:23:51 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 296 ms, Memory: 28640 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,37 +6,33 @@ class Main {
 
         int N = new Scanner(System.in).nextInt();
 
-
+        // 에라토스테네스의 체
+        // 1,000,000이 값으로 들어왔을 때 소수이면서 팰린드롬인 수 중 가장 최소값 1003001
         boolean[] A = new boolean[1003002];
         for(int i=2; i<Math.sqrt(A.length); i++){
             if(A[i]) continue;
-            for(int j = i+i; j<A.length; j += i){
+            for(int j = i + i; j<A.length; j += i){
                 A[j] = true;
             }
         }
 
+        // N이 1이라면 1을 제외시켜야함.
         for(int i = N > 1 ? N : 2; i<A.length; i++){
-            if(!A[i]){
-                if(check(i)){
-                    System.out.println(i);
-                    break;
-                }
+            if(!A[i] && isPalindrome(i)){
+                System.out.println(i);
+                break;
             }
         }
 
 
     }
 
+    public static boolean isPalindrome(int num){
+        String number = String.valueOf(num);
+        String reverse = new StringBuilder().append(num).reverse().toString();
 
-    public static boolean check(int x){
-
-        String target = String.valueOf(x);
-        String reverse = new StringBuilder().append(x).reverse().toString();
-        if(target.equals(reverse))
-            return true;
-        else{
-            return false;
-        }
+        if(number.equals(reverse)) return true;
+        else return false;
     }
 
 
