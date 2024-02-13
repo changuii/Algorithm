f99d17c35f8c901a7be2cca5a49af30dba777c1d
changui <122252160+changuii@users.noreply.github.com>
2024-02-13 19:29:50 +0900

    [Bronze I] Title: 최소공배수, Time: 140 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -9,28 +9,23 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            sb.append((a*b) / gcd(a, b) ).append("\n");
+            sb.append((a * b) / gcd(a, b)).append("\n");
         }
-
         System.out.println(sb);
     }
-    
-    public static int gcd(int a, int b){
 
-        while (a % b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
+    private static int gcd(int a, int b){
+        if(a % b == 0)
+            return b;
+        else{
+            return gcd(b, a%b);
         }
-
-        return b;
-
 
     }
 
