897ec1de80058c0298ad1779e3d33da050e53f77
changui <122252160+changuii@users.noreply.github.com>
2024-02-18 14:35:06 +0900

    [Gold IV] Title: 이분 그래프, Time: 1000 ms, Memory: 274836 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -8,71 +8,74 @@ import java.util.StringTokenizer;
 
 
 class Main {
+
     static ArrayList<Integer>[] A;
-    static boolean[] V;
+    static boolean[] visit;
+    static boolean flag;
     static int[] binary;
-    static boolean flag = false;
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      int N = Integer.parseInt(br.readLine());
-      StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
 
-      for(int i=0; i<N; i++){
-          StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-          int X = Integer.parseInt(st.nextToken())+1;
-          int E = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken()) + 1;
+            int E = Integer.parseInt(st.nextToken());
 
-          A = new ArrayList[X];
-          V = new boolean[X];
-          binary = new int[X];
-          flag = false;
-          for(int x=1; x<X; x++){
-              A[x] = new ArrayList<>();
-              binary[x] = 0;
-          }
+            A = new ArrayList[V];
+            visit = new boolean[V];
+            binary = new int[V];
+            flag = false;
 
-          for(int j=0; j<E; j++){
-              st = new StringTokenizer(br.readLine());
-              int a = Integer.parseInt(st.nextToken());
-              int b = Integer.parseInt(st.nextToken());
+            for(int j=1; j<V; j++){
+                A[j] = new ArrayList<>();
+                binary[j] = 0;
+            }
+            for(int j =0; j<E; j++){
+                st = new StringTokenizer(br.readLine() , " ");
 
-              A[a].add(b);
-              A[b].add(a);
-          }
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
 
-          for(int j=1; j<X; j++){
-              BFS(j);
-          }
-          sb.append(flag ? "NO" : "YES").append("\n");
+                A[a].add(b);
+                A[b].add(a);
+            }
 
-      }
-      System.out.println(sb);
+            for(int j=1; j<V; j++){
+                if(!visit[j]){
+                    BFS(j);
+                }
+            }
+            sb.append(flag ? "NO" : "YES").append("\n");
 
+        }
+        System.out.println(sb);
     }
+
+
     public static void BFS(int start){
-        if(V[start]) return;
         Queue<Integer> q = new LinkedList<>();
 
-        q.add(start);
-        V[start] = true;
         binary[start] = 1;
+        q.add(start);
+        visit[start] = true;
         while (!q.isEmpty()){
             int now = q.poll();
             for(int x : A[now]){
-                if(!V[x] && binary[x] == 0){
-                    q.add(x);
-                    V[x] = true;
+                if(!visit[x] && binary[x] == 0) {
                     binary[x] = binary[now] * -1;
-                }else if(V[x] && binary[x] == binary[now]) {
+                    visit[x] = true;
+                    q.add(x);
+                }else if(visit[x] && binary[x] == binary[now]){
                     flag = true;
                     return;
                 }
             }
 
         }
-
 
     }
 }
