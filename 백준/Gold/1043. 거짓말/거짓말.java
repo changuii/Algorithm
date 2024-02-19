c90f10dc0fa1a73671102e62ff42737e1deaade4
changui <122252160+changuii@users.noreply.github.com>
2024-02-19 14:25:47 +0900

    [Gold IV] Title: 거짓말, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,64 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 
 class Main {
-    static ArrayList<Integer> P;
     static int[] A;
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken())+1;
-        int M = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        for(int i=1; i<N; i++){
-            A[i] = i;
-        }
+       int N = Integer.parseInt(st.nextToken())+1;
+       int M = Integer.parseInt(st.nextToken());
+       A = new int[N];
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int p = Integer.parseInt(st.nextToken());
-        P = new ArrayList<>();
-        for(int i=0; i<p; i++){
-            P.add(Integer.parseInt(st.nextToken()));
-        }
+       for(int i=1; i<N; i++){
+           A[i] = i;
+       }
 
-        int answer = 0;
-        Queue<Integer> answerQ = new LinkedList<>();
+       st = new StringTokenizer(br.readLine(), " ");
+       int T = Integer.parseInt(st.nextToken());
+       int target = Integer.parseInt(T > 0 ? st.nextToken() : "0");
+       for(int i=1; i<T; i++){
+            union(target, Integer.parseInt(st.nextToken()));
+       }
 
-        int target = P.size() > 0 ? P.get(0) : 0;
-        for(int i=1; i<p; i++){
-            union(target, P.get(i));
-        }
+       Queue<Integer> q = new LinkedList<>();
+       for(int i=0; i<M; i++){
+           st = new StringTokenizer(br.readLine(), " ");
+           int C = Integer.parseInt(st.nextToken());
+           int first = C > 0 ? Integer.parseInt(st.nextToken()) : 0;
+           for(int j=1; j<C; j++){
+                union(first, Integer.parseInt(st.nextToken()));
+           }
+           q.add(first);
+       }
 
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int count = Integer.parseInt(st.nextToken());
+       int answer = 0;
+       while (!q.isEmpty()){
+           if(find(q.poll()) != find(target)) answer++;
+       }
 
-            Queue<Integer> q = new LinkedList();
-            for(int j=0; j<count; j++){
-                q.add(Integer.parseInt(st.nextToken()));
-            }
-
-            int value = q.poll();
-            while (!q.isEmpty()){
-                union(value, q.poll());
-            }
-            answerQ.add(value);
-        }
-
-        for(int i=0; i<M; i++){
-            if(find(target) != find(answerQ.poll())) answer++;
-        }
-        System.out.println(answer);
-
+       System.out.println(answer);
     }
-
 
     static void union(int a, int b){
         int x = find(a);
