05d1a4159c095d85c9c635502e9d90b3e13c193e
changui <122252160+changuii@users.noreply.github.com>
2024-02-20 14:53:42 +0900

    [Gold IV] Title: 최단경로, Time: 732 ms, Memory: 108988 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -6,23 +6,21 @@ import java.util.*;
 
 class Main {
     static final int INF = Integer.MAX_VALUE;
-    static int[] distance;
-    static int V;
-    static boolean[] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        V = Integer.parseInt(st.nextToken())+1;
+        int V = Integer.parseInt(st.nextToken())+1;
         int E = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(br.readLine());
 
-        visit = new boolean[V];
-        distance = new int[V];
+        boolean[] visit = new boolean[V];
+        int[] answer = new int[V];
         ArrayList<Node>[] A= new ArrayList[V];
         for(int i=1; i<V; i++){
             A[i] = new ArrayList<>();
-            distance[i] = INF;
+            if(i == K) answer[i] = 0;
+            else answer[i] = INF;
         }
 
         for(int i=0; i<E; i++){
@@ -34,30 +32,26 @@ class Main {
             A[a].add(new Node(b, w));
         }
 
-       PriorityQueue<Node> q = new PriorityQueue<>((o1, o2)->{
-           return o1.w - o2.w;
-       });
+        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> {
+            if(o1.w > o2.w) return 1;
+            else return -1;
+        });
         q.add(new Node(K, 0));
-        distance[K] = 0;
         while (!q.isEmpty()){
-            Node current = q.poll();
-            int c_v = current.b;
-            if(visit[c_v]) continue;
-            visit[c_v] = true;
-            for(int i=0; i<A[c_v].size(); i++){
-                Node tmp = A[c_v].get(i);
-                int next = tmp.b;
-                int value = tmp.w;
-                if(distance[next] > distance[c_v] + value){
-                    distance[next] = value + distance[c_v];
-                    q.add(new Node(next, distance[next]));
+            Node now = q.poll();
+            if(visit[now.b]) continue;
+            visit[now.b] = true;
+            for (Node x : A[now.b]){
+                if(answer[x.b] > x.w + now.w) {
+                    answer[x.b] = x.w + now.w;
+                    q.add(new Node(x.b, x.w + now.w));
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i=1; i<V; i++){
-            sb.append(visit[i] ? distance[i] : "INF").append("\n");
+            sb.append(visit[i] ? answer[i] : "INF").append("\n");
         }
         System.out.println(sb);
 
