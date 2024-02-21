2e15eac15ee814393e3cf12fb6aa1ff27b158ea2
changui <122252160+changuii@users.noreply.github.com>
2024-02-21 14:05:43 +0900

    [Gold IV] Title: 최단경로, Time: 732 ms, Memory: 108988 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -10,24 +10,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken())+1;
+        int V = Integer.parseInt(st.nextToken())+1;
         int E = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> {
-            return o1.weight - o2.weight;
-        });
-        boolean[] V = new boolean[N];
-        ArrayList<Node>[]A = new ArrayList[N];
-        int[] distance = new int[N];
-
-        for(int i=1; i<N; i++){
+        boolean[] visit = new boolean[V];
+        int[] answer = new int[V];
+        ArrayList<Node>[] A= new ArrayList[V];
+        for(int i=1; i<V; i++){
             A[i] = new ArrayList<>();
-            distance[i] = INF;
+            if(i == K) answer[i] = 0;
+            else answer[i] = INF;
         }
 
         for(int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
@@ -35,35 +32,39 @@ class Main {
             A[a].add(new Node(b, w));
         }
 
-        q.add(new Node(S, 0));
-        distance[S] = 0;
+        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> {
+            if(o1.w > o2.w) return 1;
+            else return -1;
+        });
+        q.add(new Node(K, 0));
         while (!q.isEmpty()){
             Node now = q.poll();
-            if(V[now.dest]) continue;
-            V[now.dest] = true;
-            for (Node x : A[now.dest]){
-                if(distance[x.dest] > distance[now.dest] + x.weight) {
-                    distance[x.dest] = distance[now.dest] + x.weight;
-                    q.add(new Node(x.dest, distance[x.dest]));
+            if(visit[now.b]) continue;
+            visit[now.b] = true;
+            for (Node x : A[now.b]){
+                if(answer[x.b] > x.w + now.w) {
+                    answer[x.b] = x.w + now.w;
+                    q.add(new Node(x.b, x.w + now.w));
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<N; i++){
-            sb.append(V[i] ? distance[i] : "INF").append("\n");
+        for(int i=1; i<V; i++){
+            sb.append(visit[i] ? answer[i] : "INF").append("\n");
         }
-
         System.out.println(sb);
 
 
     }
+
     static class Node{
-        int dest;
-        int weight;
-        Node(int dest, int weight){
-            this.dest=dest;
-            this.weight = weight;
+        int b;
+        int w;
+
+        Node(int b, int w){
+            this.b=b;
+            this.w=w;
         }
     }
 }
