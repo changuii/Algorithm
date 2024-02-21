48d752c7d5b821df3353b2e983b8aae129eedfc6
changui <122252160+changuii@users.noreply.github.com>
2024-02-21 13:10:53 +0900

    [Gold III] Title: 게임 개발, Time: 208 ms, Memory: 19680 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,55 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine())+1;
-        int[] D = new int[N];
-        ArrayList<Integer>[] A = new ArrayList[N];
-        for(int i=1; i<N; i++){
-            D[i] = 0;
-            A[i] = new ArrayList<>();
-        }
+      int N = Integer.parseInt(br.readLine())+1;
 
-        int[] time = new int[N];
-        for(int i=1; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            time[i] = Integer.parseInt(st.nextToken());
-            while (true){
-                int now = Integer.parseInt(st.nextToken());
-                if(now == -1) break;
-                A[now].add(i);
-                D[i]++;
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
+      ArrayList<Integer>[] A = new ArrayList[N];
+      int[] D = new int[N];
+      int[] time = new int[N];
         int[] answer = new int[N];
-        for(int i=1; i<N; i++){
-            int now = 1;
-            for(int j=1; j<N; j++){
-                if(D[j] == 0){
-                    now = j;
-                    D[j] = -1;
-                    break;
-                }
-            }
-            for(int x : A[now]){
-                D[x]--;
-                answer[x] = Math.max(answer[x], answer[now] + time[now]);
-            }
-        }
 
-        for(int i=1; i<N; i++){
-            sb.append(answer[i] + time[i]).append("\n");
-        }
+      for(int i=1; i<N; i++){
+          A[i] = new ArrayList<>();
+          D[i] = 0;
+          time[i] = 0;
+          answer[i] = 0;
+      }
+
+      for(int i=1; i<N; i++){
+          StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+          time[i] = Integer.parseInt(st.nextToken());
+
+          while (true){
+              int pre = Integer.parseInt(st.nextToken());
+              if(pre == -1) break;
+              A[pre].add(i);
+              D[i]++;
+          }
+      }
+
+
+      for(int i=1; i<N; i++){
+          int now = 0;
+          for(int j=1; j<N; j++){
+              if(D[j] == 0){
+                  now = j;
+                  D[j] = -1;
+                  break;
+              }
+          }
+          for (int x : A[now]){
+              D[x]--;
+              answer[x] = Math.max(answer[x], time[now] + answer[now]);
+          }
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for(int i=1; i<N; i++){
+          sb.append(answer[i] + time[i]).append("\n");
+      }
         System.out.println(sb);
+
 
     }
 }
