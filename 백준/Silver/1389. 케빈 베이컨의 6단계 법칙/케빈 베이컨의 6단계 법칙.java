7fdd62ddd880086a42f6d688548de6fb83c2863a
changui <122252160+changuii@users.noreply.github.com>
2024-02-25 16:55:11 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 136 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -5,58 +5,54 @@ import java.util.*;
 
 
 class Main {
-    static final int INF = 100000001;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st  = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken())+1;
         int M = Integer.parseInt(st.nextToken());
+        int INF = 100000001;
 
-        int[][] D = new int[N][N];
+        int[][] A = new int[N][N];
+
         for(int i=1; i<N; i++){
             for(int j=1; j<N; j++){
-                if(i == j) {
-                    D[i][j] = 0;
-                }
-                else {
-                    D[i][j] = INF;
-                }
+                if( i==j) A[i][j] = 0;
+                else A[i][j] = INF;
             }
         }
-
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            D[a][b] = 1;
-            D[b][a] = 1;
+            A[a][b] = 1;
+            A[b][a] = 1;
         }
-
 
         for(int k=1; k<N; k++){
             for(int i=1; i<N; i++){
-                for(int j=1; j<N; j++){
-                    if(D[i][j] > D[i][k] + D[k][j])
-                        D[i][j] = D[i][k] + D[k][j];
+                for(int j=1;j<N; j++){
+                    if(A[i][j] > A[i][k] + A[k][j]){
+                        A[i][j] = A[i][k] + A[k][j];
+                    }
                 }
             }
         }
 
-        int min = INF;
-        int answer = 0;
-        for(int i=1; i<N;i++){
+        int answer = 1;
+        int value = INF;
+        for(int i=1; i<N; i++){
             int sum = 0;
-            for(int j=1; j<N; j++)
-                sum += D[i][j];
-            if(min > sum) {
-                min = sum;
+            for(int j=1; j<N; j++){
+                sum += A[i][j];
+            }
+            if(value > sum){
+                value = sum;
                 answer = i;
             }
         }
-
         System.out.println(answer);
 
     }
