0a24079d9a8025c61770c343a950b55cfb835456
changui <122252160+changuii@users.noreply.github.com>
2024-03-05 11:00:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 556 ms, Memory: 69600 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -5,57 +5,49 @@ import java.util.*;
 
 
 class Main {
-    static boolean[] V;
     static ArrayList<Integer>[] A;
     static int[] answer;
+    static boolean[] V;
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
 
-       int N = Integer.parseInt(br.readLine())+1;
-       A = new ArrayList[N];
-       V = new boolean[N];
-       answer = new int[N];
-       for(int i=1; i<N; i++){
-           A[i] = new ArrayList<>();
-       }
+        int N = Integer.parseInt(br.readLine())+1;
 
-       StringTokenizer st;
-       for(int i=1; i<N-1; i++){
-           st = new StringTokenizer(br.readLine(), " ");
-           int a = Integer.parseInt(st.nextToken());
-           int b = Integer.parseInt(st.nextToken());
+        A = new ArrayList[N];
+        answer = new int[N];
+        V = new boolean[N];
 
-           A[a].add(b);
-           A[b].add(a);
-       }
+        for(int i=1; i<N; i++){
+            A[i] = new ArrayList<>();
+        }
+        StringTokenizer st;
+        for(int i=1; i<N-1; i++){
+            st = new StringTokenizer(br.readLine(), " ");
 
-       BFS(1);
-       StringBuilder sb = new StringBuilder();
-       for(int i=2; i<N; i++){
-           sb.append(answer[i]).append("\n");
-       }
-
-        System.out.println(sb);
-
-    }
-    public static void BFS(int a){
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(a);
-        V[a] = true;
-        while (!q.isEmpty()){
-            int now = q.poll();
-            for(int x : A[now]){
-                if(!V[x]){
-                    V[x] = true;
-                    q.add(x);
-                    answer[x] = now;
-                }
-            }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[a].add(b);
+            A[b].add(a);
         }
 
+        DFS(1);
+        StringBuilder sb  = new StringBuilder();
+        for(int i=2; i<N; i++){
+            sb.append(answer[i]).append("\n");
+        }
 
+        System.out.println(sb);
     }
 
+    static public void DFS(int a){
+
+        V[a] = true;
+        for(int x : A[a]){
+            if(!V[x]){
+                answer[x] = a;
+                DFS(x);
+            }
+        }
+    }
 
 }
