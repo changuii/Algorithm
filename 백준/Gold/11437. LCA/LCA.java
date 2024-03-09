b95231d9264136e6769b06cfea3310414744f43c
changui <122252160+changuii@users.noreply.github.com>
2024-03-09 16:15:02 +0900

    [Gold III] Title: LCA, Time: 500 ms, Memory: 44816 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -5,41 +5,56 @@ import java.util.*;
 
 
 class Main {
-    static int[][] D;
+    static int[] depth;
+    static int[][] parent;
+    static int K;
     static boolean[] V;
     static ArrayList<Integer>[] A;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine())+1;
+        int N = Integer.parseInt(br.readLine());
 
-        V = new boolean[N];
-        D = new int[2][N];
-        A = new ArrayList[N];
-        for(int i=1; i<N; i++){
+        int count = 1;
+        K = 0;
+        while (count <= N){
+            count <<= 1;
+            K++;
+        }
+        A = new ArrayList[N+1];
+        parent = new int[K+1][N+1];
+        V = new boolean[N+1];
+        depth = new int[N+1];
+        for(int i=1; i<N+1; i++){
             A[i] = new ArrayList<>();
         }
 
         StringTokenizer st;
-        for(int i=1; i<N-1; i++){
+        for(int i=1; i<N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
             A[a].add(b);
             A[b].add(a);
         }
         BFS(1);
 
+        for(int i = 1; i<K+1; i++){
+            for(int j=1; j<N+1; j++){
+                parent[i][j] = parent[i-1][parent[i-1][j]];
+            }
+        }
+
         int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            int a= Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             sb.append(LCA(a, b)).append("\n");
         }
         System.out.println(sb);
+
 
     }
 
@@ -52,28 +67,39 @@ class Main {
             int now = q.poll();
             for(int x : A[now]){
                 if(!V[x]){
-                    q.add(x);
                     V[x] = true;
-                    D[0][x] = now;
-                    D[1][x] = D[1][now] +1;
+                    q.add(x);
+                    depth[x] = depth[now] + 1;
+                    parent[0][x] = now;
                 }
             }
+
         }
     }
 
-    public static int LCA(int a, int b){
-        if(D[1][a] > D[1][b]){
+    static int LCA(int a, int b){
+        if(depth[a] > depth[b]){
             int temp = b;
             b = a;
             a = temp;
         }
-        while (D[1][a] != D[1][b]){
-            b = D[0][b];
+
+        int diff = depth[b] - depth[a];
+        for(int i=K; i>=0; i--){
+            if(Math.pow(2, i) <= diff){
+                b = parent[i][b];
+                diff = depth[b] - depth[a];
+            }
         }
-        while (a != b){
-            a = D[0][a];
-            b = D[0][b];
+
+        if(a == b) return a;
+        for(int i=K; i>=0; i--){
+            if(parent[i][a] != parent[i][b]){
+                a = parent[i][a];
+                b = parent[i][b];
+            }
         }
-        return a;
+        return parent[0][a];
     }
+
 }
