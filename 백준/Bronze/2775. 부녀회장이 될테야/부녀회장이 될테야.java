db6f72942b69890e1ec08e6088655895b2979358
changui <122252160+changuii@users.noreply.github.com>
2024-03-11 20:10:05 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,37 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 
 class Main {
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-       int[][] D = new int[15][15];
+        int[][] D = new int[15][15];
 
-       for(int i=1; i<D.length; i++){
-           D[0][i] = i;
-       }
+        for(int i=0; i<D.length; i++){
+            D[0][i] = i;
+        }
 
-       for(int i=1; i<D.length; i++){
-           for(int j=1; j<D.length; j++){
-               D[i][j] = D[i-1][j] + D[i][j-1];
-           }
-       }
+        for(int i=1; i<D.length; i++){
+            for(int j=1; j<D.length; j++){
+                D[i][j] = D[i][j-1] + D[i-1][j];
+            }
+        }
 
-       StringBuilder sb = new StringBuilder();
-       for(int i=0; i<T; i++){
-           int K = Integer.parseInt(br.readLine());
-           int N = Integer.parseInt(br.readLine());
-           sb.append(D[K][N]).append("\n");
-       }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<T; i++){
+            int a = Integer.parseInt(br.readLine());
+            int b = Integer.parseInt(br.readLine());
+
+            sb.append(D[a][b]).append("\n");
+        }
         System.out.println(sb);
 
-
-
     }
-
 }
