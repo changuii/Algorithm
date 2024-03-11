b7890587312326c1647127a819397b77fb36815f
changui <122252160+changuii@users.noreply.github.com>
2024-03-11 20:42:31 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 84 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
@@ -9,26 +9,25 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int M = Integer.parseInt(br.readLine());
-        int[] N = new int[M];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int T = 0;
-        for(int i=0; i<M; i++){
-           N[i] = Integer.parseInt(st.nextToken());
-           T += N[i];
-        }
-        int K = Integer.parseInt(br.readLine());
 
+        StringTokenizer st= new StringTokenizer(br.readLine(), " ");
+        int N = 0;
+        int[] color = new int[M];
+        for(int i=0; i<M; i++){
+            color[i] = Integer.parseInt(st.nextToken());
+            N += color[i];
+        }
+
+        int K = Integer.parseInt(br.readLine());
         double answer = 0;
-        for(int i=0; i<N.length; i++){
-            double value = 1.0;
+        for(int i=0; i<M; i++){
+            double value = 1;
             for(int j=0; j<K; j++){
-                value *= (double) (N[i]-j)/(T-j);
+                value *= ((double) (color[i]-j) / (N-j));
             }
             answer += value;
         }
-
         System.out.println(answer);
 
     }
-
 }
