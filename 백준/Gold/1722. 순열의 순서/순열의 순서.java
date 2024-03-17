4d10ae85076bc7fc385f34ccc40512bafda243a7
changui <122252160+changuii@users.noreply.github.com>
2024-03-17 23:40:07 +0900

    [Gold V] Title: 순열의 순서, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 
@@ -14,9 +12,9 @@ class Main {
         int N = Integer.parseInt(br.readLine());
 
         boolean[] V = new boolean[N+1];
-        long[] A = new long[N+1];
+        long[] A = new long[N];
         A[0] = 1;
-        for(int i=1; i<N+1; i++){
+        for(int i=1; i<N; i++){
             A[i] = A[i-1] * i;
         }
 
@@ -26,12 +24,12 @@ class Main {
             long th = Long.parseLong(st.nextToken()) - 1;
             for(int i=N; i > 0; i--){
                 long ith = th / A[i-1] + 1;
-                th %= A[i-1]; // th를 갱신
-                long count = 0; // 카운트하면서 ith번째 수를 찾는다.
-                int index = 0; // ith번째 수가 몇인지 찾는다.
-                while (count != ith){ // 만약 ith번째 숫자까지 왔다면 반복을 종료한다.
-                    index++; // 1 ~ N까지 돌면서 ith번째 수를 찾는다.
-                    if(!V[index]) count++; // 남아있는 숫자라면 카운트한다.
+                th %= A[i-1]; 
+                long count = 0; 
+                int index = 0;
+                while (count != ith){ 
+                    index++; 
+                    if(!V[index]) count++; 
                 }
                 V[index] = true;
                 sb.append(index).append(" ");
