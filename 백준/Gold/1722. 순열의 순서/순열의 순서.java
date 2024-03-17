437ff354f9321dcc6932aa542bb481c39597a52b
changui <122252160+changuii@users.noreply.github.com>
2024-03-17 14:10:36 +0900

    [Gold V] Title: 순열의 순서, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,66 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 
 class Main {
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-       long[] F = new long[21];
-       int[] S = new int[21];
-       boolean[] V = new boolean[21];
+        boolean[] V = new boolean[N+1];
+        long[] A = new long[N+1];
+        A[0] = 1;
+        for(int i=1; i<N+1; i++){
+            A[i] = A[i-1] * i;
+        }
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        if(st.nextToken().equals("1")){
+            long th = Long.parseLong(st.nextToken()) - 1;
+            for(int i=N; i > 0; i--){
+                long ith = th / A[i-1] + 1;
+                th %= A[i-1]; // th를 갱신
+                long count = 0; // 카운트하면서 ith번째 수를 찾는다.
+                int index = 0; // ith번째 수가 몇인지 찾는다.
+                while (count != ith){ // 만약 ith번째 숫자까지 왔다면 반복을 종료한다.
+                    index++; // 1 ~ N까지 돌면서 ith번째 수를 찾는다.
+                    if(!V[index]) count++; // 남아있는 숫자라면 카운트한다.
+                }
+                V[index] = true;
+                sb.append(index).append(" ");
+            }
+        }else{
+            long th = 1;
+            for(int i=N; i>0; i--){
+                int data = Integer.parseInt(st.nextToken());
+                int index = 0;
+                int count = 0;
+                while (index != data){
+                    index++;
+                    if(!V[index]) count++;
+                }
+                V[index] = true;
+                th += (count-1) * A[i-1];
+            }
+            sb.append(th);
+        }
 
-
-       StringTokenizer st = new StringTokenizer(br.readLine() , " ");
-       int Q = Integer.parseInt(st.nextToken());
-       F[0] = 1;
-       for(int i=1; i<= N; i++){
-           F[i] = F[i-1] * i;
-       }
-
-       StringBuilder sb = new StringBuilder();
-       // 순서 -> 값
-       if(Q == 1){
-           long K = Long.parseLong(st.nextToken());
-           // 1의 자리수부터 N자리 까지 
-           for(int i=1; i<=N; i++){
-               int cnt = 1;
-               // K가 현재 자리수에서 몇 번째인지 찾는다. ()
-               for(int j=1; j<=N; j++){
-                   if(V[j]) continue;
-                   // N = 4이고, 
-                   if(K <= cnt * F[N-i]){
-                       K -= ((cnt - 1) * F[N-i]);
-                       S[i] = j;
-                       V[j] = true;
-                       break;
-                   }
-                   cnt++;
-               }
-           }
-           for(int i=1; i<=N; i++){
-               sb.append(S[i]).append(" ");
-           }
-       // 값 -> 순서
-       }else{
-           long K = 1;
-           for(int i=1; i<= N; i++){
-               S[i] = Integer.parseInt(st.nextToken());
-               long cnt = 0;
-               for(int j=1; j<S[i]; j++){
-                   if(!V[j]) cnt++;
-               }
-               K += cnt * F[N - i];
-               V[S[i]] = true;
-           }
-           sb.append(K);
-       }
         System.out.println(sb);
+
+
     }
 
 }
