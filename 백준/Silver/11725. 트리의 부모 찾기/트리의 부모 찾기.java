c778e743b64a7c44061f162e11f01f7ee778a40c
changui <122252160+changuii@users.noreply.github.com>
2024-03-26 13:26:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 344 ms, Memory: 48256 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 
 class Main {
     static ArrayList<Integer>[] A;
     static int[] answer;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        
 
-        int N = Integer.parseInt(br.readLine())+1;
+        int N = read()+1;
         A = new ArrayList[N];
         answer = new int[N];
         for(int i=1; i<N; i++){
             A[i] = new ArrayList<>();
         }
 
-        StringTokenizer st;
         for(int i=1; i<N-1; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int a= Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a= read();
+            int b = read();
 
             A[a].add(b);
             A[b].add(a);
@@ -42,8 +37,13 @@ class Main {
                 DFS(x);
             }
         }
-
-
+    }
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 
 }
