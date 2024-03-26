d246eea9eb6a024b7e64be0228dffbf4324f41cb
changui <122252160+changuii@users.noreply.github.com>
2024-03-26 13:21:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 552 ms, Memory: 69456 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -6,14 +6,12 @@ import java.util.*;
 
 class Main {
     static ArrayList<Integer>[] A;
-    static boolean[] V;
     static int[] answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine())+1;
         A = new ArrayList[N];
-        V = new boolean[N];
         answer = new int[N];
         for(int i=1; i<N; i++){
             A[i] = new ArrayList<>();
@@ -28,30 +26,24 @@ class Main {
             A[a].add(b);
             A[b].add(a);
         }
-        BFS(1);
+        DFS(1);
 
         StringBuilder sb = new StringBuilder();
         for(int i=2; i<N; i++){
-            sb.append(answer[i] + "\n");
+            sb.append(answer[i]).append("\n");
         }
         System.out.println(sb);
 
     }
-    static void BFS(int a){
-        Queue<Integer> q = new LinkedList<>();
-        V[a] = true;
-
-        q.add(a);
-        while (!q.isEmpty()){
-            int now = q.poll();
-            for(int x : A[now]){
-                if(!V[x]){
-                    answer[x] = now;
-                    V[x] = true;
-                    q.add(x);
-                }
+    static void DFS(int a){
+        for(int x : A[a]){
+            if(answer[x] == 0){
+                answer[x] = a;
+                DFS(x);
             }
         }
+
+
     }
 
 }
