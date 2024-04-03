14348d75b7a4a5068f34234f3d14496dddc76d88
changui <122252160+changuii@users.noreply.github.com>
2024-04-03 21:15:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,55 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-
-class Main {
+public class Main {
     static int[][] A;
-    static LinkedList<Integer> l;
-    static int[] xValue = {0, 1, -1, 0};
-    static int[] yValue = {1, 0, 0, -1};
+    static boolean[][] V;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static List<Integer> answer = new ArrayList<>();
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine())+2;
+        N = Integer.parseInt(br.readLine())+2;
         A = new int[N][N];
-        l = new LinkedList<>();
-        N -= 1;
-
+        V = new boolean[N][N];
+        N--;
+        
         for(int i=1; i<N; i++){
             String value = br.readLine();
             for(int j=1; j<N; j++){
                 A[i][j] = Integer.parseInt(value.charAt(j-1)+"");
             }
         }
+
         for(int i=1; i<N; i++){
             for(int j=1; j<N; j++){
-                int value = DFS(i, j);
-                if(value != 0) l.add(value);
+                if(A[i][j] == 1 && !V[i][j]) {
+                    BFS(new Node(i, j));
+                }
             }
         }
-
+        
         StringBuilder sb = new StringBuilder();
-
-        Collections.sort(l);
-        sb.append(l.size()).append("\n");
-        for(int i=0; i<l.size(); i++){
-            sb.append(l.get(i)).append("\n");
+        sb.append(answer.size()).append("\n");
+        answer.sort((o1, o2) -> o1 - o2);
+        for(int i=0; i<answer.size(); i++){
+            sb.append(answer.get(i)).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
+        
+    }
+    static void BFS(Node now){
+        Queue<Node> q = new LinkedList<>();
+
+        int count = 1;
+        q.add(now);
+        V[now.x][now.y] = true;
+        while (!q.isEmpty()){
+            Node value = q.poll();
+            for(int i=0; i<4; i++){
+                int x = value.x + dx[i];
+                int y = value.y + dy[i];
+                if(!V[x][y] && A[x][y] == 1){
+                    q.add(new Node(x, y));
+                    count++;
+                    V[x][y] = true;
+                }
+            }
+        }
+        answer.add(count);
 
     }
 
-    public static int DFS(int x, int y){
-        if(A[x][y] == 0) return 0;
 
-        A[x][y] = 0;
-        int count = 1;
-        for(int i=0; i<4; i++){
-            count += DFS(x + xValue[i], y + yValue[i]);
+    static class Node{
+        int x;
+        int y;
+
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
         }
-        return count;
+
+        public boolean checkLocation(){
+            if(this.x == 0 || this.y == 0 || this.x == N-1 || this.y == N-1) return true;
+            return false;
+        }
     }
 
 }
