708afd1d40c2533c187e10fa19e67c13686831c2
changui <122252160+changuii@users.noreply.github.com>
2024-05-21 17:24:28 +0900

    [Gold V] Title: 로봇 청소기, Time: 132 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,101 +1,97 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 
 class Main {
-    static int[][] A;
-    static int count = 0;
-    static int[] xValue = {-1, 0, 1, 0};
-    static int[] yValue = {0, 1, 0, -1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static int[][] map;
+    // 북, 동, 남, 서
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    // 0 -> 2
+    // 1 -> 3
+    // 2 -> 0
+    // 3 -> 1
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
 
-        A = new int[N][M];
+       int N = Integer.parseInt(st.nextToken());
+       int M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        // 0 북쪽, 1 동쪽, 2 남쪽, 3 서쪽
-        int d = Integer.parseInt(st.nextToken());
+       map = new int[N][M];
 
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<M; j++){
-                A[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        BFS(new Coordinate(a, b, d));
-        System.out.println(count);
+       st = new StringTokenizer(br.readLine());
+       int startX = Integer.parseInt(st.nextToken());
+       int startY = Integer.parseInt(st.nextToken());
+       int startD = Integer.parseInt(st.nextToken());
+       Robot robot = new Robot(startX, startY, startD);
+
+       for(int i=0; i<N; i++){
+           st = new StringTokenizer(br.readLine());
+           for(int j=0; j<M; j++){
+               map[i][j] = Integer.parseInt(st.nextToken());
+           }
+       }
 
 
+       while (!robot.end){
+           clean(robot);
+       }
+
+        System.out.println(robot.count);
     }
 
-    public static void BFS(Coordinate start){
-        if(A[start.x][start.y] == 1) return;
-        Queue<Coordinate> q = new LinkedList<>();
-
-        count++;
-        A[start.x][start.y] = 2;
-        q.add(start);
-        while (!q.isEmpty()){
-            Coordinate now = q.poll();
-            int c = 0;
-            for(int i=0; i<4; i++){
-                int a = now.x + xValue[i];
-                int b = now.y + yValue[i];
-                if(A[a][b] == 0){
-                    c++;
-                    now.rotateDirection();
-                    break;
-                }
-            }
-            if(c == 0 &&
-                    (A[now.x + xValue[now.backDirection()]][now.y + yValue[now.backDirection()]] == 0 ||
-                    A[now.x + xValue[now.backDirection()]][now.y + yValue[now.backDirection()]] == 2)){
-                q.add(new Coordinate(now.x + xValue[now.backDirection()],now.y+ yValue[now.backDirection()], now.d));
-                continue;
-            }else if(c == 0 && A[now.x + xValue[now.backDirection()]][now.y + yValue[now.backDirection()]] == 1){
-                return ;
-            }
-            if(A[now.x + xValue[now.d]][now.y + yValue[now.d]] == 0){
-                A[now.x + xValue[now.d]][now.y + yValue[now.d]] = 2;
-                count++;
-                q.add(new Coordinate(now.x + xValue[now.d], now.y + yValue[now.d], now.d));
-                continue;
-            }
-            q.add(now);
+    public static void clean(Robot robot){
+        if(map[robot.x][robot.y] == 0){
+            map[robot.x][robot.y] = 2;
+            robot.count++;
         }
 
+        boolean flag = false;
+        for(int i=0; i<4; i++){
+            int valueX = robot.x + dx[i];
+            int valueY = robot.y + dy[i];
+            if(map[valueX][valueY] == 0){
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag){
+            robot.d = (robot.d + 3) % 4;
+            if(map[robot.x + dx[robot.d]][robot.y + dy[robot.d]] == 0){
+                robot.x = robot.x + dx[robot.d];
+                robot.y = robot.y + dy[robot.d];
+            }
+        }
+        else{
+            int reverseD = (robot.d + 2) % 4;
+            if(map[robot.x + dx[reverseD]][robot.y + dy[reverseD]] == 1){
+                robot.end = true;
+                return;
+            }
+            robot.x = robot.x + dx[reverseD];
+            robot.y = robot.y + dy[reverseD];
+        }
     }
 
-    static class Coordinate{
+    public static class Robot{
         int x;
         int y;
         int d;
-        public Coordinate(int x, int y, int d){
-            this.x = x;
-            this.y = y;
-            this.d = d;
-        }
-
-        public void rotateDirection(){
-            if(this.d == 0) d = 3;
-            else{
-                this.d--;
-            }
-        }
-
-        public int backDirection(){
-            if(this.d == 0) return 2;
-            if(this.d == 1) return 3;
-            if(this.d == 2) return 0;
-            return 1;
+        int count;
+        boolean end;
+        public Robot(int x, int y, int d){
+            this.x=x;
+            this.y=y;
+            this.d=d;
+            this.count = 0;
+            this.end = false;
         }
     }
+
+
 }
+
