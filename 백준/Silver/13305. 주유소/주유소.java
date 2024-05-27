9360502e0104f8f8cf511ed5b19da3bc0499915f
changui <122252160+changuii@users.noreply.github.com>
2024-05-28 00:11:52 +0900

    [Silver III] Title: 주유소, Time: 352 ms, Memory: 40532 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -17,22 +17,16 @@ class Main {
 
         int[] price = new int[N];
         st = new StringTokenizer(br.readLine());
-        price[0] = Integer.parseInt(st.nextToken());
-        ArrayList<long[]> arr = new ArrayList<>();
-        for(int i=1; i<N; i++){
+        for(int i=0; i<N; i++){
             price[i] = Integer.parseInt(st.nextToken());
-            if(price[i] <= price[i-1] || i == N-1){
-                arr.add(new long[]{price[i-1], dist[i-1]});
-            }else{
-                price[i] = price[i-1];
-                dist[i] = dist[i-1] + dist[i];
-            }
         }
 
         // 거리의 누적합을 가격이 저렴한 구간이 나오기 전까지 구한다.
         long answer = 0;
-        for(long[] x : arr){
-            answer += x[0] * x[1];
+        long now = Long.MAX_VALUE;
+        for(int i=0; i<N; i++){
+            now = Math.min(now, price[i]);
+            answer += now*dist[i];
         }
         System.out.println(answer);
     }
