d14be3349573af523555c479d96c406fef89ddac
changui <122252160+changuii@users.noreply.github.com>
2024-05-27 23:40:56 +0900

    [Silver III] Title: 주유소, Time: 348 ms, Memory: 33556 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -11,34 +11,31 @@ class Main {
 
         int[] dist = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<N; i++){
+        for(int i=0; i<N-1; i++){
             dist[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] price = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
+        price[0] = Integer.parseInt(st.nextToken());
+        ArrayList<int[]> arr = new ArrayList<>();
+        for(int i=1; i<N; i++){
             price[i] = Integer.parseInt(st.nextToken());
+            if(price[i] <= price[i-1] || i == N-1){
+                arr.add(new int[]{price[i-1], dist[i-1]});
+            }else{
+                price[i] = price[i-1];
+                dist[i] = dist[i-1] + dist[i];
+            }
         }
 
+        // 거리의 누적합을 가격이 저렴한 구간이 나오기 전까지 구한다.
+        // 
         // 1. 현재 가격보다 싼 위치를 정한다.
         // 2. 해당 위치까지 가는 데 드는 비용만큼 주유한다.
         int answer = 0;
-        for(int i=0; i<N-1; i++){
-            int min = i;
-            for(int j=i+1; j<N; j++){
-                if(price[min] >= price[j]){
-                    min = j;
-                    break;
-                }
-            }
-            int distance = 0;
-            for(int j=i+1; j<N; j++){
-                distance += dist[j];
-                if(j == min) break;
-            }
-            answer += distance*price[i];
-            i = min-1;
+        for(int[] x : arr){
+            answer += x[0] * x[1];
         }
         System.out.println(answer);
     }
