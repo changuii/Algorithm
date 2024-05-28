ec1b9e3cb619a0aba7b9aa53ca535ba5e9ad02a8
changui <122252160+changuii@users.noreply.github.com>
2024-05-28 15:06:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,11 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int divide = 100_000_000;
         while(n > 0){
-            answer += n / divide;
-            n = n % divide;
-            divide = divide/10;
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
