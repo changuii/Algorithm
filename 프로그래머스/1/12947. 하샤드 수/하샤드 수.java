99c0049e6b7b846d6a12c87ec24e570ec6a7cdd1
changui <122252160+changuii@users.noreply.github.com>
2024-05-28 15:40:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        return x % sum(x) == 0;
+    }
+    
+    public int sum(int x){
+        int sum = 0;
+        while(x > 0){
+            sum += x%10;
+            x /= 10;
+        }
+        return sum;
+    }
+}
