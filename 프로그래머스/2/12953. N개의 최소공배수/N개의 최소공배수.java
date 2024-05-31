2abee6babdb0c8f8bba5f39798aca541882d2794
changui <122252160+changuii@users.noreply.github.com>
2024-05-31 14:33:35 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.52 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] arr) {
+        Arrays.sort(arr);
+        
+        int s = arr[0];
+        for(int i=1; i<arr.length; i++){
+            int x = 1;
+            while(true){
+                if(s > arr[i]){
+                    if((x * s) % arr[i] == 0){
+                        s = x*s;
+                        break;
+                    }else x++;
+                }else{
+                    if((x * arr[i]) % s == 0){
+                        s = x* arr[i];
+                        break;
+                    }else x++;
+                }
+            }
+        }
+        return s;
+    }
+}
