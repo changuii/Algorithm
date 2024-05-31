6027b0124987fbc329a52101f603f0334e426823
changui <122252160+changuii@users.noreply.github.com>
2024-05-31 18:42:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.34 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        int[] answer = new int[score.length];
+        for(int i=0; i<score.length; i++){
+            q.add(score[i]);
+            if(q.size() > k) q.poll();
+            answer[i] = q.peek();
+        }   
+        return answer;
+    }
+}
