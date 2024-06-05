18b3dfb7eca3261db9eaf30b89c966421a9ad702
changui <122252160+changuii@users.noreply.github.com>
2024-06-05 13:45:32 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         
-        // brown + yellow = sum
-        // brown + yellow = 가로 x 세로
-        // i => 가로
-        
-        // i를 높이면서, (sum % i == 0), i >= (sum / i)
-        // ?
-        // 가로-2 * 세로-2 == yellow
-        
-        int i= 3;
-        int map = brown + yellow;
-        while(true){
-            if(map % i == 0 && i >= map / i && (i-2) * (map / i - 2) == yellow){
-                break;
-            }else i++;
+        // 10 , 2 -> 4 3
+        //  8 , 1 -> 3 3
+        // 24, 24 -> 8 6
+        // brown + yellow = width * height
+        int size = brown + yellow;
+        int width = 3;
+        while(size % width != 0 || size / width > width 
+              || yellow != (width-2) * (size / width-2)){
+            width++;
         }
+        return new int[]{width, size / width};
         
-        int[] answer = {i, map / i};
-        return answer;
     }
 }
