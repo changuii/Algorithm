c9a3407b161d3fa6229f3299324c2879f9208859
changui <122252160+changuii@users.noreply.github.com>
2024-06-05 13:56:59 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,19 +2,11 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        
-        // 6-> 3 -> (x) 2 -> 1 -> (x) 0
-        // 5 -> (x) 4 -> 2 -> 1 -> (x) 0
-        // 5000 -> 2500 -> 1250 -> 625 -> (x) 624 -> 312 -> 156 -> 78 -> 39
-        // -> (x) 38 -> 19 -> (x) 18 -> 9 -> (x)8 -> 4 -> 2 -> 1 -> (x) 0
-        int x = 0;
-        for(int i=n; i>0;){
-            if(i % 2 ==0) i = i / 2;
-            else{
-                x++;
-                i = i-1;
-            }
+        int c = 0;
+        while(n >0){
+            c += n % 2;
+            n = n /2;
         }
-        return x;
+        return c;
     }
 }
