493aa69267d5c15d8dbc2b2da54fc2f833740653
changui <122252160+changuii@users.noreply.github.com>
2024-06-05 11:01:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.63 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,17 +1,13 @@
 import java.util.*;
 class Solution
 {
-    public int solution(int []A, int []B)
-    {
-        int sum = 0;
+    public int solution(int []A, int []B){
         Arrays.sort(A);
         Arrays.sort(B);
-        
-        int j = A.length-1;
+        int sum = 0;
         for(int i=0; i<A.length; i++){
-            sum += A[i] * B[j--];
+            sum += A[i] * B[A.length-1-i];
         }
-
         return sum;
     }
 }
