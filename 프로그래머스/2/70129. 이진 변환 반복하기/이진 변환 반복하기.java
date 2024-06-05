d949a946a22980621ac745d36b820eba7eb2fe29
changui <122252160+changuii@users.noreply.github.com>
2024-06-05 11:19:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 31.24 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,34 +1,17 @@
+
 class Solution {
     public int[] solution(String s) {
         int count = 0;
-        int c =0;
+        int zero = 0;
         while(s.length() > 1){
-            // 0 제거 -> replace()를 통해 "0"을 제거하고, 
-            // 이전 값과 현재 값의 길이 차이를 통해 0의 개수를 구한다.
-            String now = s.replaceAll("0", "");
-            count += s.length() - now.length();
-            s = now;
-            
-            // 0이 제거된 문자열의 길이를 2진수로 변환해야 한다.
-            int v = s.length();
-            String convert = "";
-            // 44를 2진수로 변환하는 과정은 다음과 같다.
-            // 44
-            // 2 - 22 ... 0
-            // 2 - 11 ... 0
-            // 2 - 5  ... 1
-            // 2 - 2  ... 1
-            // 2 - 1  ... 0
-            // 2 - 0  ... 1
-            // 따라서 101100이 된다.
-            while( v >= 1){
-                convert = (v % 2) + convert;
-                v = v / 2;
-            }
-            s = convert;
-            c++;
+            int temp = s.length();
+            // 1. 0 제거
+            s = s.replaceAll("0", "");
+            zero += temp - s.length();
+            // 2. s의 길이를 2진법 문자열로
+            s = Integer.toBinaryString(s.length());
+            count++;
         }
-        
-        return new int[]{c, count};
+        return new int[]{count, zero};
     }
 }
