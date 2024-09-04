768dfa641e0dbab9524e9741bababbdfcee240ff
changui <122252160+changuii@users.noreply.github.com>
2024-09-04 16:11:05 +0900

    [D2] Title: 간단한 소인수분해, Time: 147 ms, Memory: 19,132 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -8,40 +8,24 @@ class Solution {
         int N = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
+        int[] value = new int[]{2, 3, 5, 7, 11};
         for(int i=1; i<=N; i++){
             int num = Integer.parseInt(br.readLine());
 
-            int a = 0;
-            int b = 0;
-            int c = 0;
-            int d = 0;
-            int e = 0;
+            int[] x = new int[12];
+            int val = 0;
             while(num > 1){
-                if(num % 2 == 0){
-                    num /= 2;
-                    a++;
-                }
-                if(num % 3 == 0){
-                    num /= 3;
-                    b++;
-                }
-                if(num % 5 == 0){
-                    num /= 5;
-                    c++;
-                }
-                if(num % 7 == 0){
-                    num /= 7;
-                    d++;
-                }
-                if(num % 11 == 0){
-                    num /= 11;
-                    e++;
+                if(num % value[val] == 0){
+                    x[value[val]]++;
+                    num /= value[val];
+                }else{
+                    val++;
                 }
             }
             
             
             sb.append("#" + i + " ")
-                .append(a + " " + b + " " + c+ " " + d + " " + e)
+                .append(x[value[0]] + " " + x[value[1]] + " " + x[value[2]] + " " + x[value[3]] + " " + x[value[4]])
                 .append("\n");
         }
         System.out.println(sb);
