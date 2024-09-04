8dc9d967b9ee98edb6385d2853833660eea3f932
changui <122252160+changuii@users.noreply.github.com>
2024-09-04 15:49:45 +0900

    [D3] Title: 조별과제, Time: 118 ms, Memory: 18,636 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            int a = Integer.parseInt(br.readLine());
+            sb.append("#" + i + " " + (a / 3)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
