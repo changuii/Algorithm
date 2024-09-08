d20054391da4c691327688e61b2c10c015cb75cc
changui <122252160+changuii@users.noreply.github.com>
2024-09-08 16:04:26 +0900

    [D3] Title: 전봇대, Time: 477 ms, Memory: 33,924 KB -BaekjoonHub

--- SWEA/D3/10580. 전봇대/전봇대.java
+++ SWEA/D3/10580. 전봇대/전봇대.java
@@ -20,17 +20,13 @@ class Solution {
                 int b = Integer.parseInt(st.nextToken());
                 list.add(new int[]{a, b});
             }
-            Collections.sort(list, (o1, o2)->{
-               return o1[0] - o2[0]; 
-            });
 
             int cnt = 0;
             for(int i=0; i<list.size(); i++){
                 int[] now = list.get(i);
-                for(int j=i+1; j<list.size(); j++){
-                    if(now[1] > list.get(j)[1]){
+                for(int j=0; j<list.size(); j++){
+                    if(now[0] < list.get(j)[0] && now[1] > list.get(j)[1])
                         cnt++;
-                    }
                 }
             }
             
