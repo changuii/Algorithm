56c7ecea21f0f2f0d151c8867f9b10017970cd4d
changui <122252160+changuii@users.noreply.github.com>
2024-09-14 17:37:52 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -10,31 +10,26 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] A = new int[N];
-        for(int i=0; i<N; i++){
+        int[] A = new int[N+1];
+        for(int i=1; i<=N; i++){
             A[i] = Integer.parseInt(br.readLine());
         }
 
         if(N == 1){
-            System.out.println(A[0]);
+            System.out.println(A[1]);
             return;
         }
             
-        // dp[i][j] = i번째 계단의 j번 계단횟수의 최댓값
-        int[][] dp = new int[N][2];
-        dp[0][0] = A[0];
-        dp[1][1] = A[0] + A[1];
-        dp[1][0] = A[1];
-        for(int i=2; i<N; i++){
-            dp[i][1] = dp[i-1][0] + A[i];
-            dp[i][0] = Math.max(dp[i-2][0], dp[i-2][1]) + A[i];
+        int[] dp = new int[N+1];
+        dp[1] = A[1];
+        dp[2] = A[1] + A[2];
+        // dp[i] = 2칸 전에서 온 값, 3칸 전에서 2칸뛰고 1칸 전에서 온 값 중 최댓값
+        for(int i=3; i<=N; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3] + A[i-1]) + A[i];
         }
-        
-        // for(int i=0; i<N; i++){
-        //     System.out.println(Arrays.toString(dp[i]));
-        // }
 
-        System.out.println(Math.max(dp[N-1][0], dp[N-1][1]));
+
+        System.out.println(dp[N]);
         
         
         
