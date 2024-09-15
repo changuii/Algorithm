cf09af0bf8f0341525663e6ecdc21c4da35fe9a8
changui <122252160+changuii@users.noreply.github.com>
2024-09-15 20:52:41 +0900

    [Gold V] Title: 퇴사 2, Time: 740 ms, Memory: 339340 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] A = new int[N][2];
+        StringTokenizer st;
+        
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b=  Integer.parseInt(st.nextToken());
+
+            A[i][0] = a;
+            A[i][1] = b;
+        }
+
+        // dp[i]는 i일부터 N일까지 상담했을 때의 최대값
+        long[] dp = new long[N+1];
+        for(int i=N-1; i>=0; i--){
+            if(i + A[i][0] > N )
+                dp[i] = dp[i+1];
+            else 
+                dp[i] = Math.max(dp[i+1], A[i][1] + dp[i+A[i][0]]);
+        }
+        // System.out.println(Arrays.toString(dp));
+        System.out.println(dp[0]);
+
+    }
+}
