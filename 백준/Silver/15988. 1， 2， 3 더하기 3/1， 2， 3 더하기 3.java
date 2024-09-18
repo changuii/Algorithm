b0a4e08a4a91571c1dc4dac346ab8014bc066215
changui <122252160+changuii@users.noreply.github.com>
2024-09-18 14:06:27 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 132 ms, Memory: 22212 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -17,26 +17,23 @@ class Main {
         }
 
         int divide = 1_000_000_009;
-        long[][] dp = new long[max+1][3];
+        long[] dp = new long[max+1];
 
-        dp[0][0] = 1;
-        dp[1][0] = 1;
+        dp[0] = 1;
+        dp[1] = 1;
         if(max == 1){
             System.out.println(1);
             return;
         }
-        dp[2][1] = 1;
-        dp[2][0] = 1;
+        dp[2] = 2;
         for(int i=3; i<dp.length; i++){
-            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % divide;
-            dp[i][1] = (dp[i-2][0] + dp[i-2][1] + dp[i-2][2]) % divide;
-            dp[i][2] = (dp[i-3][0] + dp[i-3][1] + dp[i-3][2]) % divide;
+            dp[i] = (dp[i-3] + dp[i-2] + dp[i-1]) % divide;
         }
 
         // System.out.println(Arrays.deepToString(dp));
         StringBuilder sb  = new StringBuilder();
         for(int i=0; i<N; i++){
-            sb.append((dp[A[i]][0] + dp[A[i]][1] + dp[A[i]][2])% divide).append("\n");
+            sb.append(dp[A[i]]).append("\n");
         }
 
         System.out.println(sb);
