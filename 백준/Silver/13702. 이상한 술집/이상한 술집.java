04b132a60acaef9b15680169e833d498938b31ce
changui <122252160+changuii@users.noreply.github.com>
2024-10-01 16:17:58 +0900

    [Silver II] Title: 이상한 술집, Time: 160 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        int max = 0;
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(br.readLine());
+            max = Math.max(A[i], max);
+        }
+
+        long l = 1;
+        long h = max;
+        long mid = 0;
+        while (l <= h) {
+            mid = (l + h) / 2;
+
+            if(check(A, M, mid)){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        System.out.println(l-1);
+
+        
+        
+      
+    }
+
+    public static boolean check(int[] A, int M, long L){
+        int c = 0;
+
+        for(int i=0; i<A.length; i++){
+            c += (int)(A[i] / L);
+        }
+
+        return c >= M;
+        
+    }
+}
