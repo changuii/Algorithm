63f98aff172cba51f170d97a9113dc3c68c6061e
changui <122252160+changuii@users.noreply.github.com>
2024-10-14 15:22:49 +0900

    [Silver V] Title: 돌 게임 2, Time: 100 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N % 2 == 0 ? "SK" : "CY");
+    }
+
+}
