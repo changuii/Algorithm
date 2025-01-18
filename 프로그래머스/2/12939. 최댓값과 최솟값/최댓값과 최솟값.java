1e09757ac322fe594c3d9817adae2560e310e04e
changui <122252160+changuii@users.noreply.github.com>
2025-01-18 17:34:58 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 15.46 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,15 +1,20 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.math.*;
-
 class Solution {
     public String solution(String s) {
-        List<Integer> list = Arrays.stream(s.split(" "))
-            .map(i->Integer.parseInt(i))
-            .collect(Collectors.toList());
-        Collections.sort(list);
+        String[] value = s.split(" ");
         
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         
-        return list.get(0) + " " + list.get(list.size()-1);
+        for(String v : value){
+            int now = Integer.parseInt(v);
+            if(min > now){
+                min = now;
+            }
+            if(max < now){
+                max = now;
+            }
+        }
+        
+        return min + " " + max;
     }
 }
