c7d33ba47b3fec25825cb9e811e0f6f44e925d32
changui <122252160+changuii@users.noreply.github.com>
2025-01-21 13:02:30 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,9 +1,25 @@
 class Solution {
     public int solution(int n) {
-        int temp = n+1;
-        while(Integer.bitCount(temp) != Integer.bitCount(n)){
-            temp++;
+        int answer = n+1;
+        int nCount = calculateConditionCount(n);
+        while(true){
+            if(satisfyedNextBigNum(nCount, answer)) break;
+            answer++;
         }
-        return temp;
+        
+        return answer;
+    }
+    
+    public boolean satisfyedNextBigNum(int nCount, int nextBigNum){
+        return nCount == calculateConditionCount(nextBigNum);
+    }
+    
+    public int calculateConditionCount(int n){
+        int c = 0;
+        while(n > 0){
+            c += n % 2;
+            n /= 2;
+        }
+        return c;
     }
 }
