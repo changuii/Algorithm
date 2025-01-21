3caa3884b21ac440e7a6ab2696d94f677bc0b62e
changui <122252160+changuii@users.noreply.github.com>
2025-01-21 13:04:40 +0900

    [level 2] Title: 피보나치 수, Time: 2.47 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
-       
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+        int[] fibo = new int[n+1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
         for(int i=2; i<=n; i++){
-            dp[i] = (dp[i-2] + dp[i-1] )% 1_234_567;
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
         }
-        return dp[n];
-       
+    
+        return fibo[n];
     }
 }
