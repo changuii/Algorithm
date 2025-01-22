72de23d37c932b241a3c150c3b55b5ff2903aee6
changui <122252160+changuii@users.noreply.github.com>
2025-01-22 16:29:57 +0900

    [level 2] Title: 할인 행사, Time: 51.63 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,44 +1,39 @@
 import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wanted = new HashMap<>();
+        int answer = 0;
         
-        int c = 0;
-        // init want
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<want.length; i++){
-            wanted.put(want[i], number[i]);
+            map.put(want[i], number[i]);
         }
         
-        // init 1일 ~ 10일
         for(int i=0; i<10; i++){
-            wanted.put(discount[i], wanted.getOrDefault(discount[i], 0) - 1);
-        }
-        
-        boolean flag = false;
-        for(String x : want){
-            if(wanted.get(x) > 0){
-                flag = true;
-                break;
-            }
-        }
-        
-        // 1일에 가입
-        if(!flag) c++;
-        
-        // 2일 ~  => 1일 제거 후 10일 추가
-        for(int i=10; i<discount.length; i++){
-            wanted.put(discount[i-10], wanted.get(discount[i-10]) + 1);
-            wanted.put(discount[i], wanted.getOrDefault(discount[i], 0) -1);
+            if(!map.containsKey(discount[i])) continue;
             
-            flag = false;
-            for(String x : want){
-                if(wanted.get(x) > 0){
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag) c++;
+            map.put(discount[i], map.get(discount[i]) - 1);
         }
-        return c;
+        if(isAllProductDiscount(map)) answer++;
+        
+        int i = 0;
+        int j = 10;
+        
+        while(j < discount.length){
+            if(map.containsKey(discount[i])) map.put(discount[i], map.get(discount[i]) + 1);
+            if(map.containsKey(discount[j])) map.put(discount[j], map.get(discount[j]) - 1);
+            
+            i++;
+            j++;
+            if(isAllProductDiscount(map)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public boolean isAllProductDiscount(Map<String, Integer> map){
+        for(String k : map.keySet()){
+            if(map.get(k) > 0) return false;
+        }
+        return true;
     }
 }
