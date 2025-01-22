2fc7ad3403e44799dcd78ab114dae555a8755de2
changui <122252160+changuii@users.noreply.github.com>
2025-01-22 15:51:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1505.37 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,24 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> set = new HashSet<>();
         
-        // i = length;
-        int c = 0;
-        for(int i=1; i<=elements.length; i++){
+        Set<Integer> count = new HashSet<>();
+        
+        for(int i=0; i<elements.length; i++){
+            
             for(int j=0; j<elements.length; j++){
-                int sum = 0;
-                for(int x=j; x<j+i; x++){
-                    int index = x;
-                    if(index >= elements.length) index %= elements.length;
-                    sum += elements[index]; 
+                int sum = 0;    
+                for(int k=0; k<=i; k++){
+                    sum += elements[(j + k) % elements.length];
                 }
-                if(!set.contains(sum)){
-                    set.add(sum);
-                    c++;
-                }
+                count.add(sum);
             }
         }
-        return c;
+        
+        return count.size();
     }
 }
