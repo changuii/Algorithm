57cd4b12a4bfb1e74083b41398c34e9a124e3b1f
changui <122252160+changuii@users.noreply.github.com>
2025-01-22 15:44:47 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,16 +1,13 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution{
+    public int solution(int n, int a, int b){
         int answer = 0;
-
-        while(a != b){
-            if(a % 2 == 0) a = a/2;
-            else a = a / 2 +1;
-            if(b % 2 == 0) b = b / 2;
-            else b = b / 2 + 1;
+        
+        while(true){
             answer++;
+            a = (a+1) / 2;
+            b = (b+1) / 2;
+            if(a == b) return answer;
         }
-        return answer;
+
     }
 }
