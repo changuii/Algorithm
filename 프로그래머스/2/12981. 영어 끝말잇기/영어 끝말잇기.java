79bccd15ebef46fb60564e993b86e9a7de257315
changui <122252160+changuii@users.noreply.github.com>
2025-01-22 15:25:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.27 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,21 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        Set<String> set = new HashSet<>();
         
-        // 0 -> 1, 1 -> 2, 2 -> 3
-        // 3 -> 1
-        int c = 0;
+        int round = -1;
+        Set<String> set = new HashSet<>();
         char before = words[0].charAt(0);
-        for(String x : words){
-            if(set.contains(x) || before != x.charAt(0)){
-                return new int[]{c % n + 1 , c / n + 1};
+        
+        for(String word : words){
+            round++;
+            
+            if(set.contains(word) || before != word.charAt(0)){
+                System.out.println(word);
+                return new int[]{round % n + 1, (round + n) / n};
             }
-            c++;
-            before = x.charAt(x.length()-1);
-            set.add(x);
+            
+            before = word.charAt(word.length()-1);
+            set.add(word);
         }
         
+
         return new int[]{0, 0};
     }
 }
