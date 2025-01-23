5c4fe49c5f8a34f78a04f7268b5766706fde75d9
changui <122252160+changuii@users.noreply.github.com>
2025-01-23 16:23:41 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 53.95 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,59 +1,54 @@
 import java.util.*;
 class Solution {
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {1, -1, 0, 0};
-    int[] count;
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    Map<Integer, Integer> map = new HashMap<>();
     public int solution(int[][] land) {
-        count = new int[250001 / 2];
-        int now = 2;
-        for(int i=0; i<land.length; i++){
-            for(int j=0; j<land[0].length; j++){
-                if(land[i][j] == 1){
-                    BFS(land, i, j, now++);
-                }
-            }
-        }
-        
-        // System.out.println(list);
-        // System.out.println(Arrays.deepToString(land));
-        
-        boolean[] v = new boolean[250001 / 2];
         int max = 0;
-        for(int i=0; i<land[0].length; i++){
-            v = new boolean[250001 / 2];
+        init(land);
+        
+        for(int j=0; j<land[0].length; j++){
+            boolean[] visit = new boolean[map.size() + 2];
             int c = 0;
-            for(int j=0; j<land.length; j++){
-                if(!v[land[j][i]] && land[j][i] > 1){
-                    c += count[land[j][i]];
-                    v[land[j][i]] = true;
+            for(int i=0; i<land.length; i++){    
+                if(land[i][j] > 0 && !visit[land[i][j]]){
+                    visit[land[i][j]] = true;
+                    c += map.get(land[i][j]);
                 }
             }
-            max = Math.max(c, max);
+            max = Math.max(max, c);
         }
-        
         return max;
     }
-    
-    public void BFS(int[][] land, int X, int Y, int w){
-        Queue<int[]> q = new ArrayDeque<>();
+    public void init(int[][] land){
+        boolean[][] visit = new boolean[land.length][land[0].length];
         
-        q.offer(new int[]{X, Y});
-        land[X][Y] = w;
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            count[w]++;
-            
-            for(int i=0; i<4; i++){
-                int x = now[0] + dx[i];
-                int y = now[1] + dy[i];
-                
-                if(!(x >=0 && x<land.length && y>=0 && y<land[0].length)) continue;
-                
-                if(land[x][y] == 1){
-                    land[x][y] = w;
-                    q.offer(new int[]{x, y});
+        int idx = 2;
+        for(int i=0; i<land.length; i++){
+            for(int j=0; j<land[0].length; j++){
+                if(!visit[i][j] && land[i][j] == 1){
+                    int c = dfs(land, i, j, idx);
+                    map.put(idx, c);
+                    idx++;
                 }
             }
         }
+    }
+    
+    
+    public int dfs(int[][] land, int x, int y, int mark){
+        land[x][y] = mark;
+        int c = 1;
+        for(int i=0; i<4; i++){
+            int X = x + dx[i];
+            int Y = y + dy[i];
+            
+            if(!(X>=0 && X<land.length && Y>=0 && Y<land[0].length)) continue;
+            
+            if(land[X][Y] == 1){
+                c += dfs(land, X, Y, mark);
+            }
+        }
+        return c;
     }
 }
