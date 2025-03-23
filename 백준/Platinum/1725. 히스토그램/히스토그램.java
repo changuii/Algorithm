f28c053f30cfbb59e18e5b6a374d24c1a9ba6055
changui <122252160+changuii@users.noreply.github.com>
2025-03-23 21:26:14 +0900

    [Platinum V] Title: 히스토그램, Time: 240 ms, Memory: 25100 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.java
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -14,13 +14,17 @@ class Main {
         for(int i=0; i<N; i++){
             int now = Integer.parseInt(br.readLine());
 
+            // 스택이 비어있거나, 탑의 높이보다 현재가 클 경우
             if(q.isEmpty() || q.peekLast()[1] < now){
                 q.addLast(new int[]{i, now});
                 continue;
             }
 
+            // 스택이 비어있지 않고, 탑의 값보다 현재 값이 작거나 같은 경우
             while(!q.isEmpty() && q.peekLast()[1] >= now){
                 int[] before = q.pollLast();
+                // 스택이 비어있는 경우 before의 높이는 before의 이전 높이들보다 작거나 같음 -> 따라서 0부터 시작하더라도 문제 없음
+                // 스택이 비어있지 않은 경우 이전 값의 우측 좌표값이 길이가 됨
                 int size = q.size() == 0 ? i : i - (q.peekLast()[0] + 1);
                 int result = before[1] * size;
 
