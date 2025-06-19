99a6579340f04b23af191e90e96fe6059d214335
changui <122252160+changuii@users.noreply.github.com>
2025-06-19 16:59:32 +0900

    [Silver II] Title: 외판원 순회 2, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,52 +1,41 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
-    static int[][] A;
     static int N;
     static int min = Integer.MAX_VALUE;
-    static boolean[] V;
-    static int INF = 1_000_001;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine())+1;
-        V = new boolean[N];
-        A = new int[N][N];
+        N = Integer.parseInt(br.readLine());
 
-        for(int i=1; i<N; i++){
-            String[] value = br.readLine().split(" ");
-            for(int j=1; j<N; j++) {
-                A[i][j] = Integer.parseInt(value[j-1].equals("0") ? INF+"" : value[j-1]);
+        int[][] map = new int[N][N];
+
+        StringTokenizer st;
+        for(int i=0; i<N; i++){
+            st  = new StringTokenizer(br.readLine());
+
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=1; i<N; i++){
-            V[i] = true;
-            DFS(1, 0, i, i);
-            V[i] = false;
-        }
+        dfs(map, 1, 0, 0);
         System.out.println(min);
-
     }
 
-    public static void DFS(int depth, int value,int start, int now){
-        if(value > min) return;
-        if(depth == N-1){
-            DFS(depth+1, value + A[now][start], start, start);
+
+    public static void dfs(int[][] map, int visit, int now, int sum){
+        if(visit == ((1 << N) - 1)){
+            if(map[now][0] == 0) return;
+            min = Math.min(min, sum + map[now][0]);
         }
 
-        if(depth == N && start == now){
-            min = value;
-        }else{
-            for(int i=1; i<N; i++){
-                if(!V[i]){
-                    V[i] = true;
-                    DFS(depth+1, value+A[now][i], start, i);
-                    V[i] = false;
-                }
+        for(int i=0; i<N; i++){
+            if(((visit & (1 << i)) == 0) && map[now][i] != 0){
+                dfs(map, visit | (1 << i), i, sum + map[now][i]);
             }
         }
-
-
     }
-
 }
