24b68e649e7ac29f6614d7202cbf031fc095edf6
changui <122252160+changuii@users.noreply.github.com>
2025-06-26 12:30:08 +0900

    [Gold IV] Title: 네트워크 연결, Time: 408 ms, Memory: 47648 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int[] value;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        value = new int[N+1];
+
+        for(int i=1; i<=N; i++){
+            value[i] = i;
+        }
+
+        StringTokenizer st;
+
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> { 
+            return o1[2] - o2[2];
+        });
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            q.add(new int[]{a, b, c});
+        }
+
+        int sum = 0;
+        int c = 0;
+        while(c < N-1){
+            int[] now = q.poll();
+
+            if(find(now[0]) != find(now[1])){
+                union(now[0], now[1]);
+                sum += now[2];
+                c++;
+            }
+        }
+        
+        System.out.println(sum);
+    }
+
+    public static void union(int a, int b){
+        int x = find(a);
+        int y = find(b);
+        
+        value[y] = x;
+    }
+
+    public static int find(int a){
+        if(value[a] == a){
+            return a;
+        }
+        return value[a] = find(value[a]);
+    }
+
+    
+}
