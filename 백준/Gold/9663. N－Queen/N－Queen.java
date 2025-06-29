e62253382abcbcf4c1b976e896e79b17e8504201
changui <122252160+changuii@users.noreply.github.com>
2025-06-29 20:16:00 +0900

    [Gold IV] Title: N-Queen, Time: 2596 ms, Memory: 15816 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,51 +4,47 @@ import java.io.*;
 
 // https://www.acmicpc.net/problem/9663
 class Main {
-    static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
-    static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
+    static int N;
     static int answer;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        int[][] visit = new int[N][N];
-        dfs(visit, 0);
+        
+        dfs(0, new boolean[N], new boolean[2 * N], new boolean[2 * N]);
 
         System.out.println(answer);
     }
 
-    public static void dfs(int[][] visit, int depth){
-        if(depth == visit.length-1){
-            for(int i=0; i<visit[0].length; i++){
-                if(visit[depth][i] == 0){
+    public static void dfs(int row, boolean[] col, boolean[] dia1, boolean[] dia2){
+        if(row == N-1){
+            for(int i=0; i<N; i++){
+                int di1 = row + i;
+                int di2 = row - i + N - 1;
+                
+                if(!col[i] && !dia1[di1] && !dia2[di2]){
                     answer++;
                 }
             }
             return;
         }
-
-        for(int i=0; i<visit[0].length; i++){
-            if(visit[depth][i] == 0){
-                check(visit, depth, i, 1);
-                dfs(visit, depth + 1);
-                check(visit, depth, i, -1);
-            }
-        }
-    }
-
-    public static void check(int[][] visit, int x, int y, int value){
-
-        for(int i=0; i<dx.length; i++){
-            int X = x + dx[i];
-            int Y = y + dy[i];
-
-            while(X >= 0 && Y >= 0 && X < visit.length && Y < visit[0].length){
-                visit[X][Y] += value;
-                X += dx[i];
-                Y += dy[i];
-            }
-        }
         
+
+        for(int i=0; i<N; i++){
+            int di1 = row + i;
+            int di2 = row - i + N - 1;
+
+            if(!col[i] && !dia1[di1] && !dia2[di2]){
+                col[i] = true;
+                dia1[di1] = true;
+                dia2[di2] = true;
+                dfs(row + 1, col, dia1, dia2);
+                col[i] = false;
+                dia1[di1] = false;
+                dia2[di2] = false;
+            }
+            
+        }
     }
 }
