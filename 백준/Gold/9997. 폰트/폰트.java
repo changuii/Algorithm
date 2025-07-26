b823951a134ec7591d8050e9ed3bb2405c9a50b0
changui <122252160+changuii@users.noreply.github.com>
2025-07-26 15:23:55 +0900

    [Gold III] Title: 폰트, Time: 220 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/9997. 폰트/폰트.java
+++ 백준/Gold/9997. 폰트/폰트.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
+// https://www.acmicpc.net/problem/9997
 class Main {
     static int isPossible = (1 << 26) - 1;
     static int answer = 0;
@@ -11,28 +11,29 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        String[] values = new String[N];
+        int[] values = new int[N];
         for(int i=0; i<N; i++){
-            values[i] = br.readLine();
+            values[i] = calculate(br.readLine());
         }
 
         dfs(values, 0, 0);
         System.out.println(answer);
     }
 
-    public static void dfs(String[] values, int now, int visit){
+    public static void dfs(int[] values, int now, int visit){
         if(visit == isPossible){
             answer++;
         }
 
         for(int i=now; i<values.length; i++){
-            dfs(values, i+1, calculateVisit(values[i], visit));
+            dfs(values, i+1, visit | values[i]);
         }
         
     }
 
-    public static int calculateVisit(String value, int visit){
+    public static int calculate(String value){
         char[] v = value.toCharArray();
+        int visit = 0;
         for(int i=0; i < v.length; i++){
             int c = v[i] - 'a';
             visit = visit | (1 << c);
