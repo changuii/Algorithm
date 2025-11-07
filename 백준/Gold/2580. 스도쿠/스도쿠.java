3f4b6929a04b63a817bb51ee774c5cf7216ebcf1
changui <122252160+changuii@users.noreply.github.com>
2025-11-07 14:24:20 +0900

    [Gold IV] Title: 스도쿠, Time: 296 ms, Memory: 21100 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -5,12 +5,20 @@ import java.io.*;
 class Main {
     static int[][] map;
     static List<int[]> list;
+
+    static boolean[][] row;
+    static boolean[][] col;
+    static boolean[][][] rc;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         map = new int[9][9];
         list = new ArrayList<>();
+
+        row = new boolean[9][10];
+        col = new boolean[9][10];
+        rc = new boolean[3][3][10];
 
         for(int i=0; i<map.length; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,6 +28,11 @@ class Main {
 
                 if(map[i][j] == 0) {
                     list.add(new int[] {i, j});
+                }
+                else {
+                    row[i][map[i][j]] = true;
+                    col[j][map[i][j]] = true;
+                    rc[i / 3][j / 3][map[i][j]] = true;
                 }
             }
         }
@@ -43,34 +56,25 @@ class Main {
         }
 
 
+        int[] now = list.get(depth);
         for(int i=1; i<=9; i++) {
-            int[] now = list.get(depth);
-
             if(isPossible(now[0], now[1], i)) {
                 map[now[0]][now[1]] = i;
+                row[now[0]][i] = true;
+                col[now[1]][i] = true;
+                rc[now[0] /3][now[1] /3][i] = true;
+
                 dfs(depth + 1);
+
+                row[now[0]][i] = false;
+                col[now[1]][i] = false;
+                rc[now[0] /3][now[1] /3][i] = false;
                 map[now[0]][now[1]] = 0;
             }
         }
     }
 
     public static boolean isPossible(int x, int y, int val) {
-        for(int i=0; i<9; i++) {
-            if(map[x][i] == val) return false;
-        }
-
-        for(int i=0; i<9; i++) {
-            if(map[i][y] == val) return false;
-        }
-
-        int mx = (x / 3) * 3;
-        int my = (y / 3) * 3;
-        for(int i=mx; i < mx + 3; i++ ){
-            for(int j=my; j < my + 3; j++ ){
-                if(map[i][j] == val) return false;
-            }
-        }
-
-        return true;
+        return !(row[x][val] || col[y][val] || rc[x / 3][y / 3][val]);
     }
 }
