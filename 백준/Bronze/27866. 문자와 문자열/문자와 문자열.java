d25b8acbccbc762122f00773e2ed82627f66a49c
changui <122252160+changuii@users.noreply.github.com>
2025-11-16 16:03:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().charAt(Integer.parseInt(br.readLine()) -1));
+    }
+}
