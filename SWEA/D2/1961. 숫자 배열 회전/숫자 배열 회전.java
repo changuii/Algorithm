2885a028fbd5868ae8948114f3b3699c9c957c6d
changui <122252160+changuii@users.noreply.github.com>
2026-01-05 21:25:22 +0900

    [D2] Title: 숫자 배열 회전, Time: 91 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -4,10 +4,9 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Solution {
-    static int max = 0;
     public static void main(String[] args) throws Exception{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
@@ -16,58 +15,34 @@ class Solution {
             int N = Integer.parseInt(br.readLine());
 
             int[][] map = new int[N][N];
-            for(int i=0; i<N; i++){
-                st= new StringTokenizer(br.readLine());
-                for(int j=0; j<N; j++){
+
+            for(int i=0; i<N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0; j<N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            int[][] one = rotate(map);
-            int[][] two = rotate(one);
-            int[][] three = rotate(two);
 
-            sb.append(String.format("#%d\n%s", t, mapToString(one, two, three)));
+            sb.append(String.format("#%d\n", t));
+
+            for(int i=0; i<N; i++) {
+                for(int j=N-1; j>=0; j--) {
+                    sb.append(map[j][i]);
+                }
+
+                sb.append(" ");
+                for(int j=N-1; j>=0; j--) {
+                    sb.append(map[N-i-1][j]);
+                }
+    
+                sb.append(" ");
+                for(int j=0; j<N; j++) {
+                    sb.append(map[j][N-i-1]);
+                }
+
+                sb.append("\n");
+            }
         }
-
         System.out.println(sb);
-        
     }
-
-    public static int[][] rotate(int[][] map){
-        int[][] rotatedMap = new int[map.length][map.length];
-
-        int x=0;
-        int y=0;
-        for(int j=0; j<map.length; j++){
-            for(int i=map.length-1; i>=0; i--){
-                rotatedMap[x][y++] = map[i][j]; 
-            }
-            y=0;
-            x++;
-        }
-        return rotatedMap;
-    }
-
-    public static String mapToString(int[][] one, int[][] two, int[][] three){
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<one.length; i++){
-            for(int j=0; j<one.length; j++){
-                sb.append(one[i][j]);
-            }
-            sb.append(" ");
-            for(int j=0; j<one.length; j++){
-                sb.append(two[i][j]);
-            }
-            sb.append(" ");
-            for(int j=0; j<one.length; j++){
-                sb.append(three[i][j]);
-            }
-            
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-    
-    
 }
